14f5a316829766468609597871c6ac7176e03d56
savitacb7557 <savitacb7557@gmail.com>
2025-12-10 07:16:53 +0000

    committed new spring basic application

---
+++ src/main/java/com/spring/practice/git/SpringPracticeGitNewApplication.java
@@ -0,0 +1,13 @@
+package com.spring.practice.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeGitNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeGitNewApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/practice/git/controller/newController.java
@@ -0,0 +1,13 @@
+package com.spring.practice.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class newController {
+
+	@GetMapping("/allow")
+	public String example() {
+		return "welcome to new spring basic application";
+	}
+}
